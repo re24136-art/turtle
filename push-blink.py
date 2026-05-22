e900116e02fdde6394320d2cf46e9333697279bc
Ibrahim Firdaus <re24136@adab.umpsa.edu.my>
2026-05-22 12:29:30 +0800

    push RGB

--- push-blink.py
+++ push-blink.py
@@ -15,17 +15,28 @@ def main():
     url = f" https://blynk.cloud/external/api/update"
 
     while True:
-        t_red = random.randint(80,90)
+        t_r = random.randint(80,90)
+        t_g = random.randint(80,90)
+        t_b = random.randint(80,90)
 
-        params = {
-            "token":keys.get('token'),
-            "V4": str(t_red)
-        }
+        params = { "token":keys.get('token'), "V4": str(t_r) }
+        resp_r = requests.get(url, params)
+        time.sleep(1)
 
-        resp = requests.get(url, params)
 
-        print(t_red, resp)
-        time.sleep(3)
+        params = { "token":keys.get('token'), "V5": str(t_g) }
+        resp_g = requests.get(url, params)
+        time.sleep(1)
+
+        params = { "token":keys.get('token'), "V6": str(t_b) }
+        resp_b = requests.get(url, params)
+        time.sleep(1)
+
+
+        print(t_r, t_g, t_b)
+        print(resp_r, resp_g, resp_b)
+
+        time.sleep(10)
 
     return
 
