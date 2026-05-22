6b801a4c3b1d34425facb6ba49a611e7871ab1c1
Ibrahim Firdaus <re24136@adab.umpsa.edu.my>
2026-05-22 14:08:24 +0800

    add push camera

--- config-camera.sh
+++ config-camera.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Disable auto-exposure on webcam and manually set exposure
-v4l2-ctl -d /dev/video0 --set-ctrl=auto_exposure=1,exposure_time_absolute=200
+v4l2-ctl -d /dev/video0 --set-ctrl=auto_exposure=1,exposure_time_absolute=100
 #,gain=128
 
 
---
+++ push-camera.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import cv2
+
+import requests
+import json
+import time
+import random
+import base64
+
+
+def capture_turtle():
+    cam = cv2.VideoCapture(0)
+    flag, frame = cam.read()
+    cam.release()
+
+    if not flag:
+        print("Camera KO")
+        return None
+
+    print(frame.shape)
+
+    # Resize image
+    scale = 0.25
+    height = int(frame.shape[0] * scale)
+    width  = int(frame.shape[1] * scale)
+
+    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+    _, image = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+    if not _:
+        print("Compression KO")
+        return None
+
+    image64 = base64.b64encode(image).decode('utf-8')
+    print(image64)
+
+    return f"data:image/jpeg;base64,{image64}"
+
+
+
+def main():
+
+    with open("api.keys", 'r') as f:
+        keys = json.load(f)
+        print(keys)
+        thingsboard_token = keys.get("thingsboard_token")
+
+
+    while True:
+
+        image = capture_turtle()
+        if image is None: continue
+
+        data = { "camera_feed": image }
+
+        url = f"http://thingsboard.cloud/api/v1/{thingsboard_token}/telemetry"
+
+        resp = requests.post(url, json=data)
+        print(resp)
+
+        time.sleep(5)
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+
+
