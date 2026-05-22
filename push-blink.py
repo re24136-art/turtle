3045198e98d1276ed5e17b84cf75f5b03f63328d
Ibrahim Firdaus <re24136@adab.umpsa.edu.my>
2026-05-22 13:08:37 +0800

    update push image

--- push-blink.py
+++ push-blink.py
@@ -12,35 +12,59 @@ def main():
         print(keys)
 
  
-    url = f" https://blynk.cloud/external/api/update"
+    #url = f" https://blynk.cloud/external/api/update"
 
     while True:
-        t_r = random.randint(80,90)
-        t_g = random.randint(80,90)
-        t_b = random.randint(80,90)
+        translucency_red   = random.randint(80,90)
+        translucency_green = random.randint(60,90)
+        translucency_blue  = random.randint(40,60)
 
-        params = { "token":keys.get('token'), "V4": str(t_r) }
-        resp_r = requests.get(url, params)
-        time.sleep(1)
+        image = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjgiIGhlaWdodD0iMTI4Ij4KPHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjRkY1NzMzIi8+CjxyZWN0IHg9IjY0IiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiMzM0M0RkYiLz4KPHJlY3QgeT0iNjQiIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iIzMzQ0Y3NyIvPgo8cmVjdCB4PSI2NCIgeT0iNjQiIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0ZGQ0YzMyIvPgo8dGV4dCB4PSI2NCIgeT0iNjQiIGZvbnQtc2l6ZT0iMTYiIGZvbnQtZmFtaWx5PSJBcmlhbCIgZmlsbD0id2hpdGUiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGRvbWluYW50LWJhc2VsaW5lPSJtaWRkbGUiPkRVTU1ZPC90ZXh0Pgo8L3N2Zz4="
+
+        data = {
+            "translucency_red":   translucency_red,
+            "translucency_green": translucency_green,
+            "translucency_blue":  translucency_blue,
+            "camera_feed": image
+        }
+
+        #curl -v -X POST http://thingsboard.cloud/api/v1/3ghpxw6ekhfqc9tsmm1e/telemetry --header Content-Type:application/json --data "{temperature:25}"
+        
+        url = "http://thingsboard.cloud/api/v1/3ghpxw6ekhfqc9tsmm1e/telemetry"
+        #--header Content-Type:application/json --data "{temperature:25}"
 
 
-        params = { "token":keys.get('token'), "V5": str(t_g) }
-        resp_g = requests.get(url, params)
-        time.sleep(1)
-
-        params = { "token":keys.get('token'), "V6": str(t_b) }
-        resp_b = requests.get(url, params)
-        time.sleep(1)
-
-
-        print(t_r, t_g, t_b)
-        print(resp_r, resp_g, resp_b)
+        resp = requests.post(url, json=data)
+        print(resp)
 
         time.sleep(10)
+        return
 
     return
 
 
 if __name__ == "__main__":
     main()
+
+
+def blynk():
+    pass
+    """
+    params = { "token":keys.get('token'), "V4": str(t_r) }
+    resp_r = requests.get(url, params)
+    time.sleep(1)
+
+
+    params = { "token":keys.get('token'), "V5": str(t_g) }
+    resp_g = requests.get(url, params)
+    time.sleep(1)
+
+    params = { "token":keys.get('token'), "V6": str(t_b) }
+    resp_b = requests.get(url, params)
+    time.sleep(1)
+    
+
+    print(t_r, t_g, t_b)
+    print(resp_r, resp_g, resp_b)
+    """
 
